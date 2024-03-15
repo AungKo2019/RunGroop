f4d19548f8d26a3709cba94f5b1ef3b62b250253
AungKo2019 <highflier.ako1991@gmail.com>
2024-03-15 10:58:31 +0630

    create and photo upload

---
+++ RunGroopWebApp/Services/PhotoServices.cs
@@ -0,0 +1,44 @@
+﻿using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using Microsoft.Extensions.Options;
+using RunGroopWebApp.Helpers;
+using RunGroopWebApp.Interfaces;
+
+namespace RunGroopWebApp.Services
+{
+    public class PhotoServices : IPhotoService
+    {
+        private readonly Cloudinary _cloudinary;
+        public PhotoServices(IOptions<CloudinarySettings> config)
+        {
+            var acc = new Account(
+                config.Value.CloudName,
+                config.Value.ApiKey,
+                config.Value.ApiSecret
+                );
+            _cloudinary = new Cloudinary( acc );
+        }
+        public async Task<ImageUploadResult> AddPhotoAsync(IFormFile file)
+        {
+            var UploadResult = new ImageUploadResult();
+            if(file.Length > 0 ) { 
+                using var stream = file.OpenReadStream();
+                var uploadParams = new ImageUploadParams
+                {
+                    File = new FileDescription(file.FileName, stream),
+                    Transformation = new Transformation().Height(500).Width(500).Crop("fill").Gravity("face")
+                };
+                UploadResult = await _cloudinary.UploadAsync(uploadParams);
+            }
+            return UploadResult;
+        }
+
+        public async Task<DeletionResult> DeletePhotoAsync(string publicId)
+        {
+            var deleteParams = new DeletionParams(publicId);
+            var result = await _cloudinary.DestroyAsync(deleteParams);
+
+            return result;
+        }
+    }
+}
