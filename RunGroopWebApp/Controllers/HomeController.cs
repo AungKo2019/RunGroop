7c997fedaf6bab1e2c3cda06c729f7604d46529d
AungKo2019 <highflier.ako1991@gmail.com>
2024-03-25 10:48:20 +0630

    add geo location

--- RunGroopWebApp/Controllers/HomeController.cs
+++ RunGroopWebApp/Controllers/HomeController.cs
@@ -1,20 +1,54 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RunGroopWebApp.Helpers;
+using RunGroopWebApp.Interfaces;
 using RunGroopWebApp.Models;
+using RunGroopWebApp.ViewModels;
 using System.Diagnostics;
+using System.Globalization;
+using System.Net;
 
 namespace RunGroopWebApp.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IClubRepository _clubRepository;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,IClubRepository clubRepository)
         {
             _logger = logger;
+            _clubRepository = clubRepository;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            var ipInfo = new IPInfo();
+            var homeVM = new HomeViewModel();
+            try
+            {
+                //string url = "ipinfo.io/103.215.193.64?token=7x6s903hj35kk";
+                string url = "https://ipinfo.io?token=99684b802260e1";
+                var info = new WebClient().DownloadString(url);
+                ipInfo = JsonConvert.DeserializeObject<IPInfo>(info);
+                RegionInfo myRI1 = new RegionInfo(ipInfo.Country);
+                ipInfo.Country = myRI1.EnglishName;
+                homeVM.City = ipInfo.City;
+                homeVM.State = ipInfo.Region;
+                if(homeVM.City != null)
+                {
+                    homeVM.Clubs = await _clubRepository.GetClubByCity(homeVM.City);
+                }
+                else
+                {
+                    homeVM.Clubs = null;
+                }
+                return View(homeVM);
+            }
+            catch (Exception ex)
+            {
+                homeVM.Clubs = null;
+            }
             return View();
         }
 
